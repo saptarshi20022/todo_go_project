f6920cfafa9b7be9825734c9c21dc6a61ea76013
saptarshi20022 <saptarshibose2002@gmail.com>
2025-01-16 00:32:06 +0530

    flags finalized

--- todo/cmd/todo.go
+++ todo/cmd/todo.go
@@ -6,6 +6,9 @@ import (
 	"fmt"
 	"os"
 	"time"
+
+	"github.com/alexeyco/simpletable"
+	
 )
 
 type item struct {
@@ -53,10 +56,50 @@ func (t *Todos) Delete(index int) error{
 
 func (t *Todos) List() {
 	
-	for i, item := range *t {
-		i++
-		fmt.Printf("%d - %s \n", i, item.Task)
+	table := simpletable.New()
+
+	table.Header = &simpletable.Header{
+		Cells: []*simpletable.Cell{
+			{Align: simpletable.AlignCenter, Text: "#"},
+			{Align: simpletable.AlignCenter, Text: "Task"},
+			{Align: simpletable.AlignCenter, Text: "Done?"},
+			{Align: simpletable.AlignRight, Text: "Created At"},
+			{Align: simpletable.AlignRight, Text: "Completed At"},
+		},
 	}
+
+	var cells [][]*simpletable.Cell
+
+	for index,item := range *t {
+		index++ // we want the index to start from 1
+
+		task := blue(item.Task)
+		done := red("NO")
+		completeTime := red("--")
+		if item.Done {
+			task = green(fmt.Sprintf("\u2705 %s", item.Task)) //adds tick mark
+			done = green("YES")
+			completeTime = green(item.CompletedAt.Format(time.RFC822))
+		}
+
+		cells = append(cells, *&[]*simpletable.Cell{ // appending the pointer to slice of simpletable cell
+			{Text: fmt.Sprintf("%d", index)}, //Sprintf formats according to a format specifier and returns the resulting string.
+			{Text: task},
+			{Text: done},
+			{Text: item.CreatedAt.Format(time.RFC822)},
+			{Text: completeTime},
+		})
+	}
+
+	table.Body = &simpletable.Body{Cells: cells}
+
+	table.Footer = &simpletable.Footer{Cells: []*simpletable.Cell{
+		{Align: simpletable.AlignCenter, Span: 5, Text: red(fmt.Sprintf("We have %d pending to-dos",t.countPending()))},
+	}}
+
+	table.SetStyle(simpletable.StyleUnicode)
+
+	table.Println()
 
 }
 
@@ -92,5 +135,16 @@ func (t *Todos) Store(fileaname string) error{
 	}
 
 	return os.WriteFile(fileaname, data, 0644) // 0644 is the permission
+}
+
+func (t *Todos) countPending() int{
+	total := 0
+	for _, item := range *t {
+		if !item.Done {
+			total++
+		}
+	}
+
+	return total
 }
 
--- todo/go.mod
+++ todo/go.mod
@@ -1,3 +1,9 @@
 module cmd-todo-app
 
 go 1.23.3
+
+require (
+	github.com/alexeyco/simpletable v1.0.0 // direct
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+)
